f176da05330f0bc04d930256f631a5b364e55a49
sistemasindustriales <66757093+sistemasindustriales@users.noreply.github.com>
2023-03-25 02:17:38 -0300

    Add files via upload

--- assets/backend/plugins/moment/locale/en-SG.js
+++ assets/backend/plugins/moment/locale/en-SG.js
@@ -9,7 +9,7 @@
 
 
     var enSG = moment.defineLocale('en-SG', {
-        months : 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
+        months : 'January_February_Marzo_April_May_June_July_August_September_October_November_December'.split('_'),
         monthsShort : 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
         weekdays : 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
         weekdaysShort : 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
